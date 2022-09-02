354e85fe3779455474b0172c479e8ba7256d7a7d
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 15:49:17 -0300

    update arrayChange.js

--- src/arrayChange.js
+++ src/arrayChange.js
@@ -17,3 +17,27 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test arrayChange
+
+// alternative solution
+// function solution(series) {
+//     let moves = 0;
+//     for (let i = 1; i < series.length; i++) {
+//         if (series[i] <= series[i - 1]) {
+//             diff = series[i - 1] - series[i] + 1;
+//             series[i] += diff;
+//             moves += diff;
+//         }
+//     }
+//     return moves;
+// }
+
+// alternative solution
+// function solution(inputArray) {
+//     return inputArray.reduce(function (x, b, i, a) {
+//         while (b >= a[i + 1]) {
+//             x++;
+//             a[i + 1]++;
+//         }
+//         return x;
+//     }, 0);
+// }
